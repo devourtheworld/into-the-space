1c96f230d09ce35ea8d0dfb34df708421e6b4f26
zebestlove <zbyslav13@gmail.com>
2020-08-04 14:29:07 +0300

    nav bar with hamburger menu and started reviews

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-function showNav(){
-    document.getElementsByClassName("navigation")[0].classList.toggle("active");
-  }
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// function showNav(){
+//     document.getElementsByClassName("navigation")[0].classList.toggle("active");
+//   }
