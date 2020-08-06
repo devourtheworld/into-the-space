6312679095ea9abc100c37e745830ad2d25f4adc
zebestlove <zbyslav13@gmail.com>
2020-08-06 21:17:37 +0300

    mars.html, products.html(almost), splited some js files

--- js/index.js
+++ js/index.js
@@ -1,34 +1,34 @@
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
-const days = document.getElementById("days");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
+// const days = document.getElementById("days");
+// const hours = document.getElementById("hours");
+// const minutes = document.getElementById("minutes");
 // const seconds = document.getElementById("seconds");
-const countdown = document.getElementById("countdown");
+// const countdown = document.getElementById("countdown");
 // const year = document.getElementById("year");
-const loading = document.getElementById("loading");
+// const loading = document.getElementById("loading");
 
-const currentYear = new Date().getFullYear();
+// const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`September 12 ${currentYear} 00:00:00`);
+// const newYearTime = new Date(`September 12 ${currentYear} 00:00:00`);
 
-//update time
-function updateCountdown() {
-  const currentTime = new Date();
-  const diff = newYearTime - currentTime;
+// //update time
+// function updateCountdown() {
+//   const currentTime = new Date();
+//   const diff = newYearTime - currentTime;
 
-  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-  const m = Math.floor(diff / 1000 / 60) % 60;
-  // const s = Math.floor(diff / 1000) % 60;
+//   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+//   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+//   const m = Math.floor(diff / 1000 / 60) % 60;
+//   // const s = Math.floor(diff / 1000) % 60;
 
-  // add values to dom
-  days.innerHTML = d;
-  hours.innerHTML = h < 10 ? "0" + h : h;
-  minutes.innerHTML = m < 10 ? "0" + m : m;
-  // seconds.innerHTML = s < 10 ? "0" + s : s;
-}
+//   // add values to dom
+//   days.innerHTML = d;
+//   hours.innerHTML = h < 10 ? "0" + h : h;
+//   minutes.innerHTML = m < 10 ? "0" + m : m;
+//   // seconds.innerHTML = s < 10 ? "0" + s : s;
+// }
 
 // show spinner before countdown                                // ?????
 // setTimeout(() => {
@@ -37,7 +37,7 @@ function updateCountdown() {
 // }, 1000);
 
 // run every second
-setInterval(updateCountdown, 1000);
+// setInterval(updateCountdown, 1000);
 
 function closeNavbar(e) {
     if (
---
+++ js/mars.js
@@ -0,0 +1,33 @@
+const km = document.getElementById("km");
+const dollars = document.getElementById("dollars");
+const times = document.getElementById("times");
+const weeks = document.getElementById("weeks");
+const kg = document.getElementById("kg");
+const add = document.getElementById("add-to-cart");
+const luxury = document.getElementById("luxury");
+const standart = document.getElementById("standart");
+const cheap = document.getElementById("cheap");
+
+function changeValues(){
+    if (document.getElementById("luxury").checked){
+        km.innerHTML = "110";
+        kg.innerHTML = "1500";
+        weeks.innerHTML = "14";
+        times.innerHTML = ">6";
+        dollars.innerHTML = "15000";
+    } else if (document.getElementById("standart").checked){
+        km.innerHTML = "110";
+        kg.innerHTML = "1000";
+        weeks.innerHTML = "10";
+        times.innerHTML = "5";
+        dollars.innerHTML = "10000";
+    } else{
+        km.innerHTML = "110";
+        kg.innerHTML = "750";
+        weeks.innerHTML = "8";
+        times.innerHTML = "3";
+        dollars.innerHTML = "7500";
+    }
+}
+
+document.querySelector('.test').addEventListener('click', changeValues);
